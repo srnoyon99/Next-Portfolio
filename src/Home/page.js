dbdc6e543dfd982db10a6f6791da1f9616b7f0f8
5599noyon <shahriarnoyon5599@gmail.com>
2026-02-01 12:27:51 +0600

    first commit

---
+++ src/Home/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepages = () => {
+  return (
+    <div>
+      hfghf
+    </div>
+  )
+}
+
+export default Homepages
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/Components/Navbar";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +20,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          <Navbar/>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
